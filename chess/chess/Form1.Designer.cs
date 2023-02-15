903cfc17627f81dcff19260ad4da66a77c1857ed
IaamSteve <kerekesistvan@turr.hu>
2023-02-15 09:33:36 +0100

    asdfg

--- chess/chess/Form1.Designer.cs
+++ chess/chess/Form1.Designer.cs
@@ -44,16 +44,16 @@ namespace chess
             // minLbl
             // 
             this.minLbl.AutoSize = true;
-            this.minLbl.Location = new System.Drawing.Point(696, 9);
+            this.minLbl.Location = new System.Drawing.Point(718, 9);
             this.minLbl.Name = "minLbl";
-            this.minLbl.Size = new System.Drawing.Size(35, 13);
+            this.minLbl.Size = new System.Drawing.Size(19, 13);
             this.minLbl.TabIndex = 0;
-            this.minLbl.Text = "label1";
+            this.minLbl.Text = "00";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(737, 9);
+            this.label2.Location = new System.Drawing.Point(743, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(10, 13);
             this.label2.TabIndex = 1;
@@ -62,11 +62,11 @@ namespace chess
             // secLbl
             // 
             this.secLbl.AutoSize = true;
-            this.secLbl.Location = new System.Drawing.Point(753, 9);
+            this.secLbl.Location = new System.Drawing.Point(759, 9);
             this.secLbl.Name = "secLbl";
-            this.secLbl.Size = new System.Drawing.Size(35, 13);
+            this.secLbl.Size = new System.Drawing.Size(19, 13);
             this.secLbl.TabIndex = 2;
-            this.secLbl.Text = "label1";
+            this.secLbl.Text = "00";
             // 
             // Form1
             // 

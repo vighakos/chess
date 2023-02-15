4906f84399f4b97263f02c9aaea127d9d57dc996
IaamSteve <kerekesistvan@turr.hu>
2023-02-15 08:35:35 +0100

    timer kész és megy is.

--- chess/chess/Form1.cs
+++ chess/chess/Form1.cs
@@ -12,6 +12,8 @@ namespace chess
 {
     public partial class Form1 : Form
     {
+        public int m = 0;
+        public int s = 0;
         public Form1()
         {
             InitializeComponent();
@@ -25,7 +27,7 @@ namespace chess
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Size = new Size(800, 640);
-
+            timer1.Start();
             for (int sor = 0; sor < 8; sor++)
                 for (int oszlop = 0; oszlop < 8; oszlop++)
                 {
@@ -47,6 +49,32 @@ namespace chess
         private void Pb_Click(object sender, EventArgs e)
         {
             
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            s++;
+            if (s == 60)
+            {
+                s = 0;
+                m += 1;
+            }
+            if (s < 10)
+            {
+                secLbl.Text = "0" + s.ToString();
+            }
+            if (s >= 10)
+            {
+                secLbl.Text = s.ToString();
+            }
+            if (m < 10)
+            {
+                minLbl.Text = "0" + m.ToString();
+            }
+            if (m >= 10)
+            {
+                minLbl.Text = m.ToString();
+            }
         }
     }
 }
